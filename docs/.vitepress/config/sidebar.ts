fd54901affd48c190038744c79b78594a98ff221
jack6 <jackz@atomeocean.com>
2025-05-26 18:28:03 -0400

    update config sidebar

---
+++ docs/.vitepress/config/sidebar.ts
@@ -0,0 +1,13 @@
+/**
+ * sidebar公共配置
+ * [vitepress-sidebar](https://github.com/jooy2/vitepress-sidebar)
+ */
+export const commonSidebarConfig =  {
+  debugPrint: true,
+  collapsed: false,
+  capitalizeFirst: true,
+  useTitleFromFileHeading: true,
+  useTitleFromFrontmatter: true,
+  useFolderTitleFromIndexFile: true,
+  sortMenusByFrontmatterOrder: true,
+};
