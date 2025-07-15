dbed67477b69f6ebf9f205e7464f5773eb782835
jack6 <jackz@atomeocean.com>
2025-07-15 12:05:01 -0400

    replace comment

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -11,6 +11,9 @@ import ContactAtomeoceanAffix from "./components/ContactAtomeoceanAffix.vue";
 import LicenseNotice from "./components/LicenseNotice.vue";
 import GoogleAds from "./components/GoogleAds.vue";
 import GiscusComment from "./components/GiscusComment.vue";
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { useData, useRoute } from 'vitepress';
+import { toRefs } from "vue";
 import {
   NolebaseGitChangelogPlugin
 } from '@nolebase/vitepress-plugin-git-changelog/client'
@@ -36,9 +39,44 @@ export default {
       app.component(key, component);
     }
   },
-  Layout() {
-    return h(DefaultTheme.Layout, null, {
-      'doc-footer-before': () => h(GiscusComment)
-    })
+  // Layout() {
+  //   return h(DefaultTheme.Layout, null, {
+  //     'doc-footer-before': () => h(GiscusComment)
+  //   })
+  // }
+  setup() {
+    // Get frontmatter and route
+    const { frontmatter } = toRefs(useData());
+    const route = useRoute();
+
+    // Obtain configuration from: https://giscus.app/
+    giscusTalk({
+        repo: 'atomeocean/atomeocean-cn-mapstruct',
+        repoId: 'R_kgDOMJ696w',
+        category: 'Announcements', // default: `General`
+        categoryId: 'DIC_kwDOMJ69684CgHTd',
+        mapping: 'pathname', // default: `pathname`
+        inputPosition: 'top', // default: `top`
+        lang: 'zh-CN', // default: `zh-CN`
+        // i18n setting (Note: This configuration will override the default language set by lang)
+        // Configured as an object with key-value pairs inside:
+        // [your i18n configuration name]: [corresponds to the language pack name in Giscus]
+        locales: {
+          'zh-Hans': 'zhHans',
+          'en-US': 'en'
+        },
+        homePageShowComment: false, // Whether to display the comment area on the homepage, the default is false
+        lightTheme: 'light', // default: `light`
+        darkTheme: 'transparent_dark', // default: `transparent_dark`
+        // ...
+      }, {
+        frontmatter, route
+      },
+      // Whether to activate the comment area on all pages.
+      // The default is true, which means enabled, this parameter can be ignored;
+      // If it is false, it means it is not enabled.
+      // You can use `comment: true` preface to enable it separately on the page.
+      true
+    );
   }
 };
